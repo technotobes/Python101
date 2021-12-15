6690919bfa6f18415f585c87364469074576e492
Toby <tobywas@gmail.com>
2021-12-15 09:55:15 -0500

    testing

--- Grocery-App-Assignment/test_grocery_app.py
+++ Grocery-App-Assignment/test_grocery_app.py
@@ -1,27 +1,28 @@
 import unittest
+from grocery_app_classes import ShoppingList
+from grocery_app_classes import GroceryItem
 
-shopping_lists = []
-
-class GroceryItem: 
-  def __init__(self, name): 
-    self.name = name
 
 
 
 class TestShoppingList(unittest.TestCase):
 
     def setUp(self):
+        self.shopping_list = ShoppingList("Walmart", "123 st")
         self.grocery_items = []
-        self.grocery_items.append(GroceryItem("eggs"))
-        self.grocery_items.append(GroceryItem("milk"))
-        self.grocery_items.append(GroceryItem("eggs"))
+        self.grocery_items.append(GroceryItem("eggs", 1, 1))
+        self.grocery_items.append(GroceryItem("milk", 1, 1))
 
-        
-        
+                
     # Test to add the same item to an existing list
     def test_adding_the_same_item(self):
-        if 2 in set([self.grocery_items.count(n) for n in self.grocery_items]):
-            self.assertTrue()
+        self.shopping_list.add_item((GroceryItem("eggs", 1, 1)))
+        print(self.grocery_items)
+        self.assertEqual(["eggs", "milk", "eggs"], self.grocery_items)
+        
+            
+    
 
 
 unittest.main()
+
