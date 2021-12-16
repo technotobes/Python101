3c9f544ffbf8be89d0705f9a6a2c4e3961ee614d
Toby <tobywas@gmail.com>
2021-12-16 16:04:10 -0500

    added comments

--- pool_table_project/util.py
+++ pool_table_project/util.py
@@ -23,6 +23,7 @@ def check_in_write_to_txt(pool_table):
     date_time_format = datetime.strptime(date_input, '%Y-%m-%d')
     new_format = str(date_time_format.strftime('%m-%d-%Y'))
     with open(f"{new_format}.txt", "a") as file:                    # Taking current date, injecting as a string 
+        # If time exceeds 60 mins, will write to text HOURS instead of minutes
         if  pool_table.total_time_minutes >= 60:
             file.write(f"Pool Table: {pool_table.table_number}\nStart Time: {pool_table.start_time}\nEnd Time: {pool_table.end_time}\nTime Played: {pool_table.formatted_hours} hour(s)\nBill: ${pool_table.formatted_cost}")
             file.write("\n")
